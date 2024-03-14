1c1ff85c9b72d0ea6dc75bfb73b467d638e45103
Ashish-Coder-Hai <161939454+Ashish-Coder-Hai@users.noreply.github.com>
2024-03-14 17:45:01 +0000

    container config

---
+++ print_given_number_prime.js
@@ -0,0 +1,12 @@
+let num=5
+let start=1
+while(start<=num){
+    if(num%1 ===0 && num%2 !==0){
+        console.log("prime");
+    }
+    else{
+        console.log("not prome number");
+    }
+
+    start++
+}
---
+++ print_sum_of_odd_number.js
@@ -0,0 +1,12 @@
+let num1=1
+let sum=0
+
+while(num1<=50){
+    if(num1%2 !==0){
+         console.log(num1)
+    }
+sum=sum+num1
+    num1++
+   
+}
+console.log(sum);

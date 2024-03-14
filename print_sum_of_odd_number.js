f62ebdb9eb47f5c63ab9c1a20dc705271d233438
Ashish-Coder-Hai <161939454+Ashish-Coder-Hai@users.noreply.github.com>
2024-03-14 18:02:28 +0000

    container config

--- print_sum_of_odd_number.js
+++ print_sum_of_odd_number.js
@@ -1,12 +1,11 @@
-let num1=1
-let sum=0
+let sp=1
+let num=50
 
-while(num1<=50){
-    if(num1%2 !==0){
-         console.log(num1)
+let sum=0
+while(sp<=num){
+    if(sp%2 !=0){
+        sum=sum+sp
     }
-sum=sum+num1
-    num1++
-   
+    sp++
 }
 console.log(sum);
